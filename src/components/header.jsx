d8af2d4b5d342c8c4621f23cf0001e52883d215a
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-08-31 16:48:58 +0500

    Header component with minor changing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import Link from "next/link";
+import "@/style_sheets/header.css"
 
 export default function Header() {
     return (
-        <header className="sticky top-0 bg-[rgb(219,158,179)]">
-            <nav className="flex sticky justify-between items-center h-[8vh] mx-auto">
+        <header className="sticky top-0 bg-gradient-to-r from-[#fff7ad] to-[#ffa9f9]">
+            <nav className="flex sticky justify-between items-center h-[8vh] mx-auto w-[70vw]">
                 <ul className="flex p-0 items-center">
-                    <li>
-                        <Link href="/">
-                            <h3>Pony Party</h3>
+                    <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px]">
+                        <Link href="/" className="flex flex-col items-center justify-center">
+                            <h3 className="text-[18px]">Pony Party</h3>
+                            <p className="text-[10px]">IV HYDRATION</p>
                         </Link>
                     </li>
                 </ul>
                 <ul className="flex p-0 items-center">
-                    <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px] rounded-full">
-                        <Link href="/">Home</Link>
+                    <li className="inline-block text-black text-[14px] py-[4px] mx-[6px] rounded-full">
+                        <Link href="/">In-Home Services</Link>
                     </li>
-                    <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px] rounded-full">
-                        <Link href="/Projects" className="Projects">Projects</Link>
+                    <li className="inline-block text-black text-[14px] py-[4px] mx-[8px] rounded-full">
+                        <Link href="/Projects" className="Projects">Shipped To You</Link>
                     </li>
-                    <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px] rounded-full">
-                        <Link href="/AboutMe" className="AboutMe">About</Link>
+                    <li className="inline-block text-black text-[14px] py-[4px] mx-[8px] rounded-full">
+                        <Link href="/AboutMe" className="AboutMe">Community</Link>
                     </li>
-                    <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px] rounded-full">
-                        <Link href="/Contact" className="Contact">Contact Us</Link>
+                    <li className="inline-block text-black text-[14px] py-[4px] mx-[8px] rounded-full">
+                        <Link href="/Contact" className="Contact">Memberships</Link>
+                    </li>
+                    <li className="inline-block text-black text-[14px] py-[4px] mx-[8px] rounded-full">
+                        <Link href="/Contact" className="Contact">About Us</Link>
                     </li>
                 </ul>
                 <ul className="flex p-0 items-center">
                     <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px] rounded-full">
                         <Link href="/Login">
-                            <button>Log in</button>
+                            <button className="bg-">Log in</button>
                         </Link>
                     </li>
                 </ul>
