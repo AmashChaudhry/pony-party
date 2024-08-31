b007a6a15335cfc3a2861d3a77e1b57e9dfec1f7
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-08-31 23:41:33 +0500

    Minor changes

--- src/app/pages/login-to-account/page.jsx
+++ src/app/pages/login-to-account/page.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import Link from "next/link";
 
 export default function LoginToAccount() {
-    return(
+    return (
         <div>
             <form className="justify-between items-center mx-auto w-[70vw] p-[50px]">
-                <div className="mb-[15px]">
-                    <label className="text-black text-opacity-60">Email address (as your login)</label><br />
-                    <input className="w-[400px] p-[15px] text-[16px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none" type="email" required />
-                </div>
-                <div className="mb-[15px]">
-                    <label className="text-black text-opacity-60">Password</label><br />
-                    <input className="w-[400px] p-[15px] text-[16px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none" type="password" required />
-                </div>
-                <div className="mb-[15px]">
-                    <input className="mr-[10px]" type="checkbox" id="remember-me" />
-                    <label htmlFor="remember-me">Remember me</label>
-                </div>
-                <button className="bg-[#ffa9f9] text-white py-[15px] px-[20px] hover:bg-black" type="submit">Login</button>
-                <div className="mt-[15px]">
-                    <a href="/forgot-password" className="text-[#ffa9f9]">Forgot password?</a><br /><br />
-                    <p>Don’t have an account? <Link href="/pages/register-account" className="text-[#ffa9f9]">Register now</Link></p>
+                <div className="w-[400px]">
+                    <div className="mb-[15px]">
+                        <label className="text-black text-opacity-60">Email address (as your login)</label><br />
+                        <input className="w-[400px] p-[15px] text-[16px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none" type="email" required />
+                    </div>
+                    <div className="mb-[15px]">
+                        <label className="text-black text-opacity-60">Password</label><br />
+                        <input className="w-[400px] p-[15px] text-[16px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none" type="password" required />
+                    </div>
+                    <div className="mb-[15px]">
+                        <input className="mr-[10px]" type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <button className="bg-[#ffa9f9] text-white py-[15px] px-[20px] hover:bg-black" type="submit">Login</button>
+                    <div className="mt-[15px]">
+                        <a href="/forgot-password" className="text-[#ffa9f9]">Forgot password?</a><br /><br />
+                        <p>Don’t have an account? <Link href="/pages/register-account" className="text-[#ffa9f9]">Register now</Link></p>
+                    </div>
                 </div>
             </form>
         </div>
