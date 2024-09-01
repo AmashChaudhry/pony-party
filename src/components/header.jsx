0a1997e19bfc794690646cede40e75cd5f658f1b
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-09-01 21:25:34 +0500

    Responsive Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuPosition, setMenuPosition] = useState("top-[-100vh]");
+
+    useEffect(() => {
+        if (menuOpen) {
+            setMenuPosition("top-[8vh]");
+        } else {
+            setMenuPosition("top-[-100vh]");
+        }
+    }, [menuOpen]);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className="sticky top-0 bg-gradient-to-r from-[#fff7ad] to-[#ffa9f9]">
-            <nav className="flex sticky justify-between items-center h-[8vh] mx-auto w-[70vw]">
+            <nav className="flex sticky justify-between items-center h-[8vh] mx-auto lg:w-[70vw]">
+                <button className="text-[12px] p-[15px] mx-[15px] bg-black rounded-md text-white lg:hidden" name="menu" onClick={toggleMenu}>{menuOpen ? "Close" : "Menu"}</button>
                 <ul className="flex p-0 items-center">
                     <li className="inline-block text-black text-[14px] py-[4px] px-[10px] mx-[5px]">
                         <Link href="/" className="flex flex-col items-center justify-center">
@@ -13,25 +30,27 @@ export default function Header() {
                         </Link>
                     </li>
                 </ul>
+                <div className={`lg:static absolute lg:bg-transparent bg-white border-b border-gray-300 lg:min-h-fit min-h-fit left-0 ${menuPosition} lg:w-auto w-full flex items-center px-5 py-[20px]`}>
+                    <ul className="flex lg:flex-row flex-col lg:p-0 lg:items-center lg:gap-0 gap-8">
+                        <li className="inline-block text-black  text-[14px] lg:text-[12px] py-[4px] mx-[6px]">
+                            <Link href="/">In-Home Services</Link>
+                        </li>
+                        <li className="inline-block text-black  text-[14px] lg:text-[12px] py-[4px] mx-[6px]">
+                            <Link href="/Projects" className="Projects">Shipped To You</Link>
+                        </li>
+                        <li className="inline-block text-black  text-[14px] lg:text-[12px] py-[4px] mx-[6px]">
+                            <Link href="/AboutMe" className="AboutMe">Community</Link>
+                        </li>
+                        <li className="inline-block text-black  text-[14px] lg:text-[12px] py-[4px] mx-[6px]">
+                            <Link href="/Contact" className="Contact">Memberships</Link>
+                        </li>
+                        <li className="inline-block text-black  text-[14px] lg:text-[12px] py-[4px] mx-[6px]">
+                            <Link href="/Contact" className="Contact">About Us</Link>
+                        </li>
+                    </ul>
+                </div>
                 <ul className="flex p-0 items-center">
-                    <li className="inline-block text-black text-[12px] py-[4px] mx-[6px] rounded-full">
-                        <Link href="/">In-Home Services</Link>
-                    </li>
-                    <li className="inline-block text-black text-[12px] py-[4px] mx-[8px] rounded-full">
-                        <Link href="/Projects" className="Projects">Shipped To You</Link>
-                    </li>
-                    <li className="inline-block text-black text-[12px] py-[4px] mx-[8px] rounded-full">
-                        <Link href="/AboutMe" className="AboutMe">Community</Link>
-                    </li>
-                    <li className="inline-block text-black text-[12px] py-[4px] mx-[8px] rounded-full">
-                        <Link href="/Contact" className="Contact">Memberships</Link>
-                    </li>
-                    <li className="inline-block text-black text-[12px] py-[4px] mx-[8px] rounded-full">
-                        <Link href="/Contact" className="Contact">About Us</Link>
-                    </li>
-                </ul>
-                <ul className="flex p-0 items-center">
-                    <li className="inline-block text-black text-[12px] py-[4px] px-[10px] mx-[5px] rounded-full">
+                    <li className="inline-block text-black text-[14px] py-[4px] mx-[15px]">
                         <Link href="/pages/login-to-account">Log in</Link>
                     </li>
                 </ul>
