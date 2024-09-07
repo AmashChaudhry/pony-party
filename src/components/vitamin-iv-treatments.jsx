3fe8e4db7161b9c96a883bd34652670eebff7ff3
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-09-07 19:28:45 +0500

    Minor changes

--- src/components/vitamin-iv-treatments.jsx
+++ src/components/vitamin-iv-treatments.jsx
@@ -18,7 +18,7 @@ export default function VitaminIVTreatments() {
                         />
                     </div>
                     <Link className="text-[10px] text-center font-bold hover:text-[#ffa9f9] sm:text-[12px] xl:text-[14px]" href={`/pages/vitamin-iv-treatment/${treatment.id}`}>
-                        {`${treatment.title} - ${treatment.price}`}
+                        {`${treatment.title} - $${treatment.price}`}
                     </Link>
                     <div className="h-[2px] w-[80px] bg-[#ffa9f9] my-[10px]"></div>
                     <p className="text-[10px] text-center text-black text-opacity-60 h-[40px] mb-[30px] sm:text-[12px] xl:text-[14px]">
