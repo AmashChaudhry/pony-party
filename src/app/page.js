31690d87dd5e6484d4a3232040b911db8fe23ebf
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-09-14 05:16:48 +0500

    About us update

--- src/app/page.js
+++ src/app/page.js
@@ -147,6 +147,45 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <span className="h-0 w-full sm:h-[60px]"></span>
+      <div className="flex flex-col items-center w-full py-[40px]">
+                <h2 className="text-[24px] font-medium">What Make us Different</h2>
+                <div className="flex flex-wrap justify-center mx-auto max-w-[1200px]">
+                    <div className="flex flex-col items-center h-fit w-[45%] px-[10px] my-[60px] sm:w-[310px]">
+                        <div className="relative h-[100px] w-full m-[10px]">
+                            <Image
+                                src="/about-us/map.png"
+                                fill
+                                style={{ objectFit: "contain" }}
+                                alt="Calender"
+                            />
+                        </div>
+                        <h3 className="text-[14px] text-center font-bold hover:text-[#ffa9f9] mt-[20px] sm:text-[16px]">80+ Cities Worldwide</h3>
+                    </div>
+                    <div className="flex flex-col items-center h-fit w-[45%] px-[10px] my-[60px] sm:w-[310px]">
+                        <div className="relative h-[100px] w-full m-[10px]">
+                            <Image
+                                src="/about-us/medical.png"
+                                fill
+                                style={{ objectFit: "contain" }}
+                                alt="Calender"
+                            />
+                        </div>
+                        <h3 className="text-[14px] text-center font-bold hover:text-[#ffa9f9] mt-[20px] sm:text-[16px]">Expert Medical Team</h3>
+                    </div>
+                    <div className="flex flex-col items-center h-fit w-[45%] px-[10px] my-[60px] sm:w-[310px]">
+                        <div className="relative h-[100px] w-full m-[10px]">
+                            <Image
+                                src="/about-us/reviews.png"
+                                fill
+                                style={{ objectFit: "contain" }}
+                                alt="Calender"
+                            />
+                        </div>
+                        <h3 className="text-[14px] text-center font-bold hover:text-[#ffa9f9] mt-[20px] sm:text-[16px]">3,000+ 5-Star Reviews</h3>
+                    </div>
+                </div>
+            </div>
     </main>
   );
 }
--- src/app/pages/about-us/page.jsx
+++ src/app/pages/about-us/page.jsx
@@ -9,7 +9,7 @@ export default function AboutUs() {
                 <p className="text-[16px] text-center text-gray-600">Pony Party is the global leader of in-home IV therapy and medical services.</p>
             </div>
             <div className="flex flex-col items-center bg-[#FAFAFA] w-full py-[40px]">
-                <h2 className="text-[24px]">What Make us Different</h2>
+                <h2 className="text-[24px] font-medium">What Make us Different</h2>
                 <div className="flex flex-wrap justify-center mx-auto max-w-[1200px]">
                     <div className="flex flex-col items-center h-fit w-[45%] px-[10px] my-[60px] sm:w-[310px]">
                         <div className="relative h-[100px] w-full m-[10px]">
@@ -49,7 +49,26 @@ export default function AboutUs() {
             <div className="flex flex-col items-center px-6 py-[40px]">
                 <h2 className="text-[22px] font-semibold">Our Story</h2>
                 <div className="bg-[#ffa9f9] h-[2px] w-[120px] mt-[5px] mb-[20px]"></div>
-                <p className="text-center max-w-[1000px]">Two best friends with a passion for health, wellness, and healing. For those of you who work hard and like to play hard we have the ultimate recovery packages! We know that when you feel your best you perform even better, which is why we offer a variety of nutrients to help boost your mood, energy, and overall health. With an extensive background and years of clinical experience as nurses we are committed to providing the safest quality of care to each of our patients. At Pony Party we strive to deliver excellence, convenience, and comfort with all of our mobile services.</p>
+                <p className="text-[16px] text-center max-w-[1000px]">Two best friends with a passion for health, wellness, and healing. For those of you who work hard and like to play hard we have the ultimate recovery packages! We know that when you feel your best you perform even better, which is why we offer a variety of nutrients to help boost your mood, energy, and overall health. With an extensive background and years of clinical experience as nurses we are committed to providing the safest quality of care to each of our patients. At Pony Party we strive to deliver excellence, convenience, and comfort with all of our mobile services.</p>
+            </div>
+            <span className="h-[60px]"></span>
+            <div className="flex flex-col items-center w-full max-w-[1000px] px-[20px] sm:flex-row sm:items-start">
+                <div className="w-full h-full sm:w-[60%]">
+                    <Image
+                        src="/about-us/nurse.jpg"
+                        width={350}
+                        height={350}
+                        className="w-full h-auto"
+                        style={{ objectFit: "cover" }}
+                        alt="Nurse"
+                    />
+                </div>
+                <div className="flex flex-col w-full mt-10 sm:ml-10 sm:mt-0">
+                    <h3 className="text-[20px] font-medium mb-4">Our Nurse</h3>
+                    <p className="text-[16px]">
+                        Our Registered Nurses are thoroughly vetted, highly trained, and fully licensed, bringing extensive experience from their work in both ER and ICU settings. Our team of medical professionals is dedicated to delivering the safest and highest quality care to every patient. At Drip Hydration, we are committed to excellence in medical service delivery, with safety being our top priority in everything we do.
+                    </p>
+                </div>
             </div>
         </div>
     );
