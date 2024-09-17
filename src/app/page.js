4030d72241058e86721323df7bc756f4aab4055e
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-09-17 10:32:39 +0500

    Minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FaRegCheckCircle } from "react-icons/fa";
 import VitaminIVTreatments from "@/components/vitamin-iv-treatments";
 
 export default function Home() {
@@ -48,17 +49,97 @@ export default function Home() {
       </div>
       <VitaminIVTreatments />
       <span className="h-[30px] w-full lg:h-[60px]"></span>
-      <div className="flex flex-col items-start w-full max-w-[1200px]">
-        <h1 className="text-[30px] mb-10">What Is IV Nutritional Therapy?</h1>
-        <p className="mb-5">
-          IV nutritional therapy, also known as intravenous therapy, IV nutrition, or IV nutrient therapy, is a treatment widely recognized for its numerous health benefits. These may include anti-aging effects, enhanced immune function, reduced anxiety, alleviation of hangover symptoms, and more. Despite the common belief that nutrient deficiencies are rare today, many individuals still lack the essential nutrients needed for optimal body performance.        </p>
-        <p className="mb-5">
-          Since IV treatment is administered directly into the bloodstream, it can deliver faster results compared to oral supplements or other traditional medications. Many physicians consider IV nutrient therapy to be safe, effective, and restorative, with the ability to be tailored to the individual needs of each patient.
-        </p>
-        <p className="mb-5">
-          As a supplier of IV vitamin therapy and an IV pharmacy, Pony Party offers a wide range of IV kits designed to meet diverse needs. Explore our full selection of available IV kits online.
-        </p>
-        <h1 className="text-[26px] mb-10">Possible Benefits of IV Nutritional Therapy:</h1>
+      <div className="flex flex-col items-center w-full bg-[#FAFAFA] py-[60px]">
+        <div className="flex flex-col items-center w-full max-w-[1200px] px-[20px] md:flex-row">
+          <div className="w-full h-full mb-10 md:mb-0">
+            <Image
+              src="/home/iv-drip-vitamin-infusion-therapy-saline-bag_144356-61806.jpg"
+              width={350}
+              height={350}
+              className="w-full h-auto"
+              style={{ objectFit: "cover" }}
+              alt="Nurse"
+            />
+          </div>
+          <div className="flex flex-col items-start w-full md:ml-10">
+            <h1 className="text-[30px] mb-10">What Is IV Nutritional Therapy?</h1>
+            <p className="mb-5">
+              IV nutritional therapy, also known as intravenous therapy, IV nutrition, or IV nutrient therapy, is a treatment widely recognized for its numerous health benefits. These may include anti-aging effects, enhanced immune function, reduced anxiety, alleviation of hangover symptoms, and more. Despite the common belief that nutrient deficiencies are rare today, many individuals still lack the essential nutrients needed for optimal body performance.        </p>
+            <p className="mb-5">
+              Since IV treatment is administered directly into the bloodstream, it can deliver faster results compared to oral supplements or other traditional medications. Many physicians consider IV nutrient therapy to be safe, effective, and restorative, with the ability to be tailored to the individual needs of each patient.
+            </p>
+            <p className="mb-10">
+              As a supplier of IV vitamin therapy and an IV pharmacy, Pony Party offers a wide range of IV kits designed to meet diverse needs. Explore our full selection of available IV kits online.
+            </p>
+            <h1 className="text-[26px] mb-10">Possible Benefits of IV Nutritional Therapy:</h1>
+            <div className="flex flex-col justify-start w-full lg:flex-row">
+              <div className="mr-[40px]">
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Boosts energy</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Detoxifies the body</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Enhances the immune system</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Helps prevent illness</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Decreases artery plaque</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Boosts energy</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Detoxifies the body</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Enhances the immune system</p>
+                </div>
+                <div className="flex flex-row items-center mb-[10px]">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Helps prevent illness</p>
+                </div>
+                <div className="flex flex-row items-center">
+                  <div className="w-[30px] h-[30px]">
+                    <FaRegCheckCircle size={30} className="text-[#ffa9f9]" />
+                  </div>
+                  <p className="ml-[30px]">Decreases artery plaque</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <span className="h-0 w-full sm:h-[60px]"></span>
       <div className="flex flex-col items-center w-full py-[40px]">
