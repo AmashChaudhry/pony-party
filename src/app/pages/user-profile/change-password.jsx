63596cd0cb5949f68eb60ab1990e53a95d72d899
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-09-20 09:46:55 +0500

    Minor changes

--- src/app/pages/user-profile/change-password.jsx
+++ src/app/pages/user-profile/change-password.jsx
@@ -1,22 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 export default function ChangePassword() {
-    const [user, setUser] = useState(null);
-
-    const getUserData = async () => {
-        const response = await fetch('/api/current-user', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        const userData = await response.json();
-        setUser(userData.data);
-    };
-
-    useEffect(() => {
-        getUserData();
-    }, []);
+    const [user, setUser] = useState({
+        currentPassword: "",
+        newPassword: "",
+        confirmNewPassword: "",
+    });
 
     return (
         <div className='w-full max-w-[500px] px-[20px]'>
@@ -26,9 +15,9 @@ export default function ChangePassword() {
                         <label className="text-black text-opacity-60">Current Password</label><br />
                         <input className="w-full p-[15px] text-[14px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none"
                             type="password"
-                            id="password"
-                            value={user.password}
-                            onChange={(e) => setUser({ ...user, password: e.target.value })}
+                            id="currentPassword"
+                            value={user.currentPassword}
+                            onChange={(e) => setUser({ ...user, currentPassword: e.target.value })}
                             required
                         />
                     </div>
@@ -37,6 +26,8 @@ export default function ChangePassword() {
                         <input className="w-full p-[15px] text-[14px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none"
                             type="password"
                             id="newPassword"
+                            value={user.newPassword}
+                            onChange={(e) => setUser({ ...user, newPassword: e.target.value })}
                             required
                         />
                     </div>
@@ -45,9 +36,17 @@ export default function ChangePassword() {
                         <input className="w-full p-[15px] text-[14px] bg-[rgba(0,0,0,0.05)] border-l-4 border-l-[#ffa9f9] focus:outline-none"
                             type="password"
                             id="confirmNewPassword"
+                            value={user.confirmNewPassword}
+                            onChange={(e) => setUser({ ...user, confirmNewPassword: e.target.value })}
                             required
                         />
                     </div>
+                    <button
+                        className='bg-[#ffa9f9] hover:bg-black text-white w-fit py-[15px] px-[25px] mt-[10px]'
+                        type="submit"
+                    >
+                        Save Changes
+                    </button>
                 </div>
                 : <div className=' flex flex-col items-center w-full'>
                     <h1>Loading...</h1>
