f8c8865b92297677032cd013c3c0c49b6f75d04e
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-10-01 18:26:00 +0500

    Minor changes

--- src/app/api/change-password/route.js
+++ src/app/api/change-password/route.js
@@ -23,6 +23,10 @@ export async function PATCH(request) {
             return NextResponse.json({ error: "Current password is incorrect." }, { status: 400 });
         }
 
+        if (currentPassword !== newPassword) {
+            return NextResponse.json({ error: "Please try different password." }, { status: 400 });
+        }
+
         user.password = newPassword;
         await user.save();
 
