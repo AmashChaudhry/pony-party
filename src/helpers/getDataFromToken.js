7e14ecc5fda2608e6c19ff97282601374654b6d5
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-10-04 17:22:58 +0500

    Minor changes

--- src/helpers/getDataFromToken.js
+++ src/helpers/getDataFromToken.js
@@ -1,34 +1,33 @@
-import jwt from "jsonwebtoken";
-
-export const getDataFromToken = (request) => {
-    try {
-        const token = request.cookies.get("token")?.value || "";
-        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-        return decodedToken.id;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
-
-
-
 // import jwt from "jsonwebtoken";
 
 // export const getDataFromToken = (request) => {
 //     try {
-//         // Ensure this runs server-side only
-//         if (!request.cookies) {
-//             throw new Error("No cookies found");
-//         }
-        
 //         const token = request.cookies.get("token")?.value || "";
-//         if (!token) {
-//             throw new Error("Token not found");
-//         }
-        
 //         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
 //         return decodedToken.id;
 //     } catch (error) {
 //         throw new Error(error.message);
 //     }
-// };
+// }
+
+
+
+import jwt from "jsonwebtoken";
+
+export const getDataFromToken = (request) => {
+    try {
+        if (!request.cookies) {
+            throw new Error("No cookies found");
+        }
+
+        const token = request.cookies.get("token")?.value || "";
+        if (!token) {
+            throw new Error("Token not found");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+        return decodedToken.id;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
