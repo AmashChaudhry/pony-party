374577b21cdb7683e68b89fd0e3c1c41aa609a62
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-10-04 17:05:14 +0500

    Minor changes

--- src/app/api/current-user/route.js
+++ src/app/api/current-user/route.js
@@ -1,20 +1,3 @@
-// import User from "@/models/userModel";
-// import { connect } from "@/lib/mongodb_config";
-// import { NextResponse } from "next/server";
-// import { getDataFromToken } from "@/helpers/getDataFromToken";
-
-// connect();
-
-// export async function GET(request) {
-//     const userId = await getDataFromToken(request);
-//     const user = await User.findOne({_id: userId}).select("-password");
-//     return NextResponse.json({
-//         message: "User found",
-//         data: user,
-//     });
-// }
-
-
 import User from "@/models/userModel";
 import { connect } from "@/lib/mongodb_config";
 import { NextResponse } from "next/server";
@@ -23,26 +6,43 @@ import { getDataFromToken } from "@/helpers/getDataFromToken";
 connect();
 
 export async function GET(request) {
-    try {
-        const userId = await getDataFromToken(request);
-
-        const user = await User.findOne({ _id: userId }).select("-password");
-
-        if (!user) {
-            return NextResponse.json(
-                { message: "User not found" },
-                { status: 404 }
-            );
-        }
-
-        return NextResponse.json({
-            message: "User found",
-            data: user,
-        });
-    } catch (error) {
-        return NextResponse.json(
-            { message: error.message || "An error occurred" },
-            { status: 500 }
-        );
-    }
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({_id: userId}).select("-password");
+    return NextResponse.json({
+        message: "User found",
+        data: user,
+    });
 }
+
+
+// import User from "@/models/userModel";
+// import { connect } from "@/lib/mongodb_config";
+// import { NextResponse } from "next/server";
+// import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+// connect();
+
+// export async function GET(request) {
+//     try {
+//         const userId = await getDataFromToken(request);
+
+//         const user = await User.findOne({ _id: userId }).select("-password");
+
+//         if (!user) {
+//             return NextResponse.json(
+//                 { message: "User not found" },
+//                 { status: 404 }
+//             );
+//         }
+
+//         return NextResponse.json({
+//             message: "User found",
+//             data: user,
+//         });
+//     } catch (error) {
+//         return NextResponse.json(
+//             { message: error.message || "An error occurred" },
+//             { status: 500 }
+//         );
+//     }
+// }
