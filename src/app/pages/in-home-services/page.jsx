46946855eb023c529f437cf5fccd8cfc1d923035
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-10-07 18:16:34 +0500

    Minor changes

--- src/app/pages/in-home-services/page.jsx
+++ src/app/pages/in-home-services/page.jsx
@@ -7,7 +7,7 @@ export default function InHomeServices() {
         <div className="flex flex-col items-center">
             <div className="flex flex-col items-center p-[40px] sm:py-[60px] sm:pb-[60px] sm:w-[700px] lg:py-[80px] lg:pb-[90px] xl:py-[100px] xl:pb-[150px]">
                 <h1 className="text-[26px] text-center font-medium py-[20px] sm:text-[34px]">Mobile Medical Service Menu</h1>
-                <p className="text-[16px] text-center text-gray-500 font-medium sm:text-[18px]">Drip Hydration offers a variety of at home services. Learn more about what we offer below.</p>
+                <p className="text-[16px] text-center text-gray-500 font-medium sm:text-[18px]">Pony Party IV offers a variety of at home services. Learn more about what we offer below.</p>
             </div>
             <div className="flex flex-col items-center p-[30px]">
                 <h1 className="text-[20px] text-center font-medium tracking-widest sm:text-[24px]">VITAMIN IV THERAPY</h1>
