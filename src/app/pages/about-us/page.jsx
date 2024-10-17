12b7d588330aed3d3ee5879fa38cd19064bcdb8b
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2024-10-18 00:43:28 +0500

    Minor changes

--- src/app/pages/about-us/page.jsx
+++ src/app/pages/about-us/page.jsx
@@ -49,9 +49,6 @@ export default function AboutUs() {
             <div className="flex flex-col items-center w-full max-w-[1000px] px-[20px] py-[40px]">
                 <h2 className="text-[22px] font-semibold">Our Story</h2>
                 <div className="bg-[#ffa9f9] h-[2px] w-[120px] mt-[5px] mb-[20px]"></div>
-                <p className="max-w-[1000px]">
-                    We understand firsthand that when you feel your best, you are unstoppable. Your energy is higher, your mood is brighter, and your overall health transforms. At Pony Party, we are not just offering treatments; we are offering the chance for you to reclaim your vitality, one carefully curated service at a time. Our range of nutrient-rich therapies is designed to boost your body and soul, so you can return to doing the things you love, feeling more alive than ever.
-                </p>
             </div>
             <div className="flex flex-col items-center w-full max-w-[1000px] px-[20px] sm:flex-row sm:items-start">
                 <div className="w-full h-full">
@@ -61,7 +58,7 @@ export default function AboutUs() {
                         alt="Nurse"
                     />
                 </div>
-                <div className="flex flex-col w-full mt-[40px] sm:ml-10 sm:mt-0">
+                <div className="flex flex-col w-full my-[40px] sm:ml-10 sm:mt-0">
                     <p className="text-[16px]">
                         We are two best friends with a passion for health, wellness, and healing. For
                         those of you who work hard and like to play hard we have the ultimate recovery
@@ -76,11 +73,6 @@ export default function AboutUs() {
                         excellence, convenience, and comfort with all of our services.
                     </p>
                 </div>
-            </div>
-            <div className='w-full max-w-[1000px] px-[20px] py-[40px]'>
-                <p>
-                    At Pony Party, our mission is simple: to bring excellence, convenience, and comfort directly to you. We understand that life moves fast, and that is why our mobile services are designed to fit seamlessly into your world. Whether you need recovery, a mood boost, or simply some time to feel taken care of, we are here for you—ready to bring wellness wherever you are.
-                </p>
             </div>
             <span className='h-[40px]'></span>
             <div className="flex flex-col items-start w-full max-w-[1000px] px-[20px]">
